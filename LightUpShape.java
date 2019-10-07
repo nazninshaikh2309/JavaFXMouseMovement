4f76ea28d0937c073c5aa7a24cffa98a3c51c7cf
nazninshaikh2309 <56244556+nazninshaikh2309@users.noreply.github.com>
2019-10-06 22:43:54 -0700

    Add files via upload
    Randomly change color of rectangle when mouse moved over it. And change color to transparent when mouse is moved outside the bound of rectangle

---
+++ LightUpShape.java
@@ -0,0 +1,53 @@
+
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class LightUpShape extends Application {
+	private final static int X_CORD = 150;
+	private final static int Y_CORD = 150;
+	private final static int WIDTH = 200;
+	private final static int HEIGHT= 200;
+	private Rectangle rectangle;
+	public void start(Stage primaryStage) {
+		
+		Group group = new Group();
+		
+		rectangle = new Rectangle(X_CORD, Y_CORD, WIDTH, HEIGHT);
+		rectangle.setFill(Color.TRANSPARENT);
+		rectangle.setStroke(Color.TRANSPARENT);
+		group.getChildren().add(rectangle);
+		
+		Scene scene = new Scene(group, 500, 500, javafx.scene.paint.Color.BISQUE);
+		scene.setOnMouseMoved(this :: processMouse);
+		primaryStage.setTitle("Light up A Shape");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	
+	public void processMouse(MouseEvent event) {
+		int x = (int)event.getX();
+		int y = (int)event.getY();
+		if (x>=X_CORD &&  x <= WIDTH + X_CORD && y <= HEIGHT + Y_CORD  && y>=Y_CORD)
+		{
+			
+			rectangle.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+		}
+		else
+		{
+		
+			rectangle.setFill(Color.TRANSPARENT);
+			rectangle.setStroke(Color.BLACK);
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
